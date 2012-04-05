cfc71f7ef1e08ef9383f646e71076dd1fff44b6c
Andrew Brookins <a.m.brookins@gmail.com>
2012-04-05 06:23:29 -0700

    Use a key binding

--- vintage.py
+++ vintage.py
@@ -993,10 +993,6 @@ class ViSetBookmark(sublime_plugin.TextCommand):
 
 class ViSelectBookmark(sublime_plugin.TextCommand):
     def run(self, edit, character, select_bol=False):
-        if character == '.':
-            self.view.run_command('vi_goto_last_edit')
-            return
-
         self.view.run_command('select_all_bookmarks', {'name': "bookmark_" + character})
         if select_bol:
             sels = list(self.view.sel())
@@ -1098,13 +1094,13 @@ LAST_EDITS_SETTING = 'last_edits'
 
 class ViRecordLastEdit(sublime_plugin.EventListener):
     def on_modified(self, view):
-        last_edits = view.settings().get(LAST_EDITS_SETTING, {}) 
-        edit_position = view.sel()[0] 
+        last_edits = view.settings().get(LAST_EDITS_SETTING, {})
+        edit_position = view.sel()[0]
         last_edits[str(view.id())] = {'a': edit_position.a, 'b': edit_position.b}
-        view.settings().set(LAST_EDITS_SETTING, last_edits) 
+        view.settings().set(LAST_EDITS_SETTING, last_edits)
 
- 
-class ViGotoLastEdit(sublime_plugin.TextCommand): 
+
+class ViGotoLastEdit(sublime_plugin.TextCommand):
     # The position the cursor was at before the command fired. Saved when the
     # command is run, so that if the user runs the command again before making
     # another edit in the file, the cursor returns to its original position.
@@ -1113,12 +1109,12 @@ class ViGotoLastEdit(sublime_plugin.TextCommand):
     def move_cursor_to_region(self, region):
         """ Clear the cursor's position and move it to `region`. """
         cursor = self.view.sel()
-        self.original_position = cursor[0] 
+        self.original_position = cursor[0]
         cursor.clear()
         cursor.add(region)
-        self.view.show(region) 
+        self.view.show(region)
 
-    def run(self, edit): 
+    def run(self, edit):
         """
         If there was a last edit recorded for the view, store the current
         position as self.original_position and move the cursor to the position
@@ -1127,7 +1123,7 @@ class ViGotoLastEdit(sublime_plugin.TextCommand):
         If the cursor is currently at the same position as the last edit, and
         there `self.original_position` is available, then return the cursor to
         its original position.
-        """ 
+        """
         last_edits = self.view.settings().get(LAST_EDITS_SETTING, {})
         last_edit = last_edits.get(str(self.view.id()), None)
         current_position = self.view.sel()[0]

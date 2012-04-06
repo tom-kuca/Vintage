f441fb16587cb95424b1549c2865dd13b1bb1fa3
Andrew Brookins <a.m.brookins@gmail.com>
2012-04-05 21:58:35 -0700

    Use View.get_regions() and View.add_regions() to store last edits

--- vintage.py
+++ vintage.py
@@ -1089,15 +1089,13 @@ class MoveGroupFocus(sublime_plugin.WindowCommand):
             return
 
 
-LAST_EDITS_SETTING = 'last_edits'
+LAST_EDIT_KEY = 'vintage_last_edit'
 
 
 class ViRecordLastEdit(sublime_plugin.EventListener):
     def on_modified(self, view):
-        last_edits = view.settings().get(LAST_EDITS_SETTING, {})
-        edit_position = view.sel()[0]
-        last_edits[str(view.id())] = {'a': edit_position.a, 'b': edit_position.b}
-        view.settings().set(LAST_EDITS_SETTING, last_edits)
+        """ Record the cursor's position at the last edit in the view. """
+        view.add_regions(LAST_EDIT_KEY, [s for s in view.sel()], '')
 
 
 class ViGotoLastEdit(sublime_plugin.TextCommand):
@@ -1106,37 +1104,33 @@ class ViGotoLastEdit(sublime_plugin.TextCommand):
     # another edit in the file, the cursor returns to its original position.
     original_position = None
 
-    def move_cursor_to_region(self, region):
-        """ Clear the cursor's position and move it to `region`. """
-        cursor = self.view.sel()
-        self.original_position = cursor[0]
-        cursor.clear()
-        cursor.add(region)
-        self.view.show(region)
+    def move_cursor_to_region_set(self, region_set):
+        """ Clear the cursor's position and move it to `region_set`. """
+        sel = self.view.sel()
+        self.original_position = [r for r in sel]
+        sel.clear()
+        for region in region_set:
+            sel.add(region)
 
-    def run(self, edit):
+    def run(self, edit): 
         """
-        If there was a last edit recorded for the view, store the current
-        position as self.original_position and move the cursor to the position
+        If there was a last edit recorded for the view, save the current
+        position as `self.original_position` and move the cursor to the position
         of the last edit.
 
         If the cursor is currently at the same position as the last edit, and
         there `self.original_position` is available, then return the cursor to
         its original position.
         """
-        last_edits = self.view.settings().get(LAST_EDITS_SETTING, {})
-        last_edit = last_edits.get(str(self.view.id()), None)
-        current_position = self.view.sel()[0]
+        last_edit = self.view.get_regions(LAST_EDIT_KEY)
+        current_position = [r for r in self.view.sel()]
 
         if last_edit is None:
-            return
-
-        last_edit_region = sublime.Region(
-            long(last_edit['a']), long(last_edit['b']))
+            return 
 
         if self.original_position is not None \
-            and current_position == last_edit_region:
-            self.move_cursor_to_region(self.original_position)
+                and current_position == last_edit:
+            self.move_cursor_to_region_set(self.original_position)
             return
 
-        self.move_cursor_to_region(last_edit_region)
+        self.move_cursor_to_region_set(last_edit)
